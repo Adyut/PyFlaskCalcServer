44e43f638de7ce2f762c08d58345522217a7bdf3
Adyut <adyut123@gmail.com>
2021-06-06 19:40:35 +0530

    Cors enabled server

--- CalcServer.py
+++ CalcServer.py
@@ -1,14 +1,18 @@
 from flask import Flask, jsonify, json, request
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 from CalcOps import SimpleOps as so
 
 calc = Flask(__name__)
-CORS(calc)
+cors = CORS(calc)
+calc.config['CORS_HEADERS'] = 'Content-Type'
 
 
 @calc.route("/<op_name>", methods=['POST'])
+@cross_origin()
 def operation(op_name):
     data = json.loads(request.data)
+    # op_name = data['op']
+    print(f"Received Operation Request for '{op_name}' with arguments num1 : {data['num1']} and num2 : {data['num2']}.")
     result = 'Error'
     if 'add' == op_name:
         result = so.add(data['num1'], data['num2'])
@@ -19,8 +23,9 @@ def operation(op_name):
     elif 'sub' == op_name:
         result = so.sub(data['num1'], data['num2'])
     elif 'neg' == op_name:
-        result = so.mul(data['num1'], -1)
+        result = so.mul(data['num2'], -1)
 
+    print(f"Response is {result}.")
     return jsonify(result)
 
 
